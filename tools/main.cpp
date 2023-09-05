c9b3052275547103c9498f93736aa82848c0aaf1
Orangels <orangels0313@163.com>
2023-09-05 15:25:43 +0800

    Init main.cpp
    Signed-off-by: Orangels <orangels0313@163.com>

---
+++ tools/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cout<<("Dispatch Server start\n")<<endl;
+//    CConfiger* pConfiger = CConfiger::getOrCreateConfiger("../cfg/configer.txt");
+    
+    return 0;
+}
